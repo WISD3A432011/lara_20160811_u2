876587dbc39f7e6d00e60a0e38730deb4cb5a777
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-12 15:08:53 +0800

    練習7-設定回傳預設首頁

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//class HomeController extends Controller
+//{
+    //
+   // public function index()
+    //{
+        //return'index';
+    //}
+//}
 class HomeController extends Controller
 {
     //
     public function index()
     {
-        return'index';
+        returnview('welcome');
     }
 }
